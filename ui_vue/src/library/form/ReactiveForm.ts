3c82d5cdb825db663ad77b052a1e11be7aa7ea9c
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-13 14:55:45 +0900

    Create ReactiveForm

---
+++ ui_vue/src/library/form/ReactiveForm.ts
@@ -0,0 +1,33 @@
+import { ReactiveFormControl } from './ReactiveFormControl';
+
+export default class ReactiveForm {
+  public controls: ReactiveFormControl[] = [];
+
+  public validate() {
+    for (const key in this.controls) {
+      this.controls[key].validate();
+    }
+  }
+
+  public get errors() {
+    let retval: any = {}
+
+    for (const key in this.controls) {
+      if (this.controls[key].errors != null) {
+        retval[key] = this.controls[key].errors;
+      }
+    }
+    return retval;
+  }
+
+  public get hasErrors() {
+    let retval: number = 0;
+
+    for (const key in this.controls) {
+      if (this.controls[key].errors != null) {
+        retval += this.controls[key].errors.length;
+      }
+    }
+    return retval;
+  }
+}
