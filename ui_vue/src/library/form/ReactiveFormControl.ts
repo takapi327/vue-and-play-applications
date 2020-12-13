001afe9eb3e71542bbe2d5ce0ca89e2007e311a9
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-13 14:55:29 +0900

    Create ReactiveFormControl

---
+++ ui_vue/src/library/form/ReactiveFormControl.ts
@@ -0,0 +1,28 @@
+export class ReactiveFormControl {
+  constructor(
+    public  value:      any,
+    private validators: Function[] = []
+  ) {}
+
+  public errors: any[] = [];
+
+  public reset() {
+    this.value  = null;
+    this.errors = [];
+  }
+
+  public validate() {
+    this.errors = [];
+
+    if (!this.validators || !this.validators.length) return;
+
+    this.validators.forEach(validator => {
+      const error = validator.call(this, this.value);
+
+      if (error != null) {
+        if (!this.errors) this.errors = [];
+        this.errors.push(error);
+      }
+    })
+  }
+}
