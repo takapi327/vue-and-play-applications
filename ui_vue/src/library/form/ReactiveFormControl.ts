4a20cf7f4d54170492f8453c08f877fe4b94f2de
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-13 16:13:47 +0900

    Delete semicoron

--- ui_vue/src/library/form/ReactiveFormControl.ts
+++ ui_vue/src/library/form/ReactiveFormControl.ts
@@ -1,27 +1,25 @@
 export class ReactiveFormControl {
+
   constructor(
     public  value:      any,
     private validators: Function[] = []
   ) {}
 
-  public errors: any[] = [];
+  public errors: any[] = []
 
   public reset() {
-    this.value  = null;
-    this.errors = [];
+    this.value  = null
+    this.errors = []
   }
 
   public validate() {
-    this.errors = [];
-
+    this.errors = []
     if (!this.validators || !this.validators.length) return;
-
     this.validators.forEach(validator => {
-      const error = validator.call(this, this.value);
-
+      const error = validator.call(this, this.value)
       if (error != null) {
-        if (!this.errors) this.errors = [];
-        this.errors.push(error);
+        if (!this.errors) this.errors = []
+        this.errors.push(error)
       }
     })
   }
