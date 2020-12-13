8c7657259567c2f76ec449d925158946d0c00ec4
takapi327 <i-happy-go-lucky-life0001@ezweb.ne.jp>
2020-12-13 16:16:15 +0900

    Create chatRegister/membership/Step1.vue

---
+++ ui_vue/src/components/chatRegister/membership/Step1.vue
@@ -0,0 +1,40 @@
+<template>
+  <form>
+    <input v-model="formData.controls.name.value" type="text" />
+  </form>
+  <button v-on:click="click">OK</button>
+</template>
+
+<script lang="ts">
+import { defineComponent     } from 'vue'
+import { ReactiveFormControl } from '@/library/form/ReactiveFormControl'
+
+import ReactiveForm            from '@/library/form/ReactiveForm'
+import Validators              from '@/library/form/validators/Validators'
+
+export default defineComponent({
+  name: 'Step1',
+
+  setup() {
+    let formData: ReactiveForm = new ReactiveForm();
+
+    formData.controls['name'] = new ReactiveFormControl(
+      '',
+      [Validators.required, Validators.stringLength(3, 128)]
+    )
+
+    return {
+      formData
+    }
+  },
+
+  methods: {
+    click: function (event: any) {
+      console.log(this.formData.controls.name.value)
+    }
+  }
+})
+</script>
+
+<style scoped lang="scss">
+</style>
